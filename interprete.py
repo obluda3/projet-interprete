8d5ad28e28fd5cfe9e6dac917977fbd71e8c6112
Slaytt <117762113+Slaytt@users.noreply.github.com>
2023-10-31 06:49:01 -0400

    Add files via upload

---
+++ interprete.py
@@ -0,0 +1,74 @@
+class interprete:
+    def __init__(self):
+        self.op  = { 
+                    "add": self.add,
+                    "minus": self.minus,
+                    "div": self.div,
+                    "mult": self.times,
+                    "while": self.while_sequence,
+                    "if": self.if_sequence,
+                    "set": self.set_variable,
+                    "output": self.output_value,
+                    "more": self.more_cond,
+                    "equal": self.equal_cond,
+                    "less": self.less_cond
+                }
+        self.variables = {}
+    
+    def eval(self,operand):
+        if type(operand) == int:
+            return operand
+        if type(operand) == str:
+            return self.variables[operand]
+        return self.exec_inst(operand)
+
+    def add(self,operands):
+        return eval(operands[0]) + eval(operands[1])
+
+    def minus(self,operands):
+        return self.eval(operands[0]) - self.eval(operands[1])
+
+    def times(self,operands):
+        return self.eval(operands[0]) * self.eval(operands[1])
+
+    def div(self,operands):
+        return self.eval(operands[0]) // self.eval(operands[1])
+
+    def exec_inst(self,instruction):
+        inst = instruction[0]
+        f = self.op[inst]
+        return f(instruction[1:])
+
+    def exec_sequence(self,seq):
+        for line in seq[1:]:
+            self.exec_inst(line)
+
+    def while_sequence(self,args):
+        cond = args[0]
+        seq = args[1]
+        while eval(cond):
+            self.exec_sequence(seq)
+
+    def if_sequence(self,args):
+        cond = args[0]
+        seq = args[1]
+        if self.eval(cond):
+            self.exec_sequence(seq)
+
+    def set_variable(self,args):
+        name = args[0]
+        value = self.eval(args[1])
+        self.variables[name] = value
+
+    def more_cond(self,args):
+        return self.eval(args[0]) > self.eval(args[1])
+
+    def less_cond(self,args):
+        return self.eval(args[0]) < self.eval(args[1])
+
+    def equal_cond(self,args):
+        return self.eval(args[0]) == self.eval(args[1])
+
+    def output_value(self,args):
+        print(self.eval(args[0]))
+
--- main.py
+++ main.py
@@ -1,81 +1,18 @@
+from interprete import *
+def main():
+    code =  ["sequence", 
+            ["set", "i", 500],
+            ["set", "fizz", 100],
+            ["if", ["less", ["div", "i", 1000], "fizz"], 
+                ["sequence", ["output", "i"]]]
+            ]
+    test = interprete()
+    #with open("shell",encoding = 'utf-8') as file: 
+    test.exec_sequence(code)
+
+if __name__== '__main__' :
+    main()
 
 
-def eval(operand):
-    if type(operand) == int:
-        return operand
-    if type(operand) == str:
-        return variables[operand]
-    return exec_inst(operand)
 
-def add(operands):
-    return eval(operands[0]) + eval(operands[1])
 
-def minus(operands):
-    return eval(operands[0]) - eval(operands[1])
-
-def times(operands):
-    return eval(operands[0]) * eval(operands[1])
-
-def div(operands):
-    return eval(operands[0]) // eval(operands[1])
-
-def exec_inst(instruction):
-    inst = instruction[0]
-    f = d[inst]
-    return f(instruction[1:])
-
-def exec_sequence(seq):
-    for line in seq[1:]:
-        exec_inst(line)
-
-def while_sequence(args):
-    cond = args[0]
-    seq = args[1]
-    while eval(cond):
-        exec_sequence(seq)
-
-def if_sequence(args):
-    cond = args[0]
-    seq = args[1]
-    if eval(cond):
-        exec_sequence(seq)
-
-def set_variable(args):
-    name = args[0]
-    value = eval(args[1])
-    variables[name] = value
-
-def more_cond(args):
-    return eval(args[0]) > eval(args[1])
-
-def less_cond(args):
-    return eval(args[0]) < eval(args[1])
-
-def equal_cond(args):
-    return eval(args[0]) == eval(args[1])
-
-def output_value(args):
-    print(eval(args[0]))
-
-d = { 
-    "add": add,
-    "minus": minus,
-    "div": div,
-    "mult": times,
-    "while": while_sequence,
-    "if": if_sequence,
-    "set": set_variable,
-    "output": output_value,
-    "more": more_cond,
-    "equal": equal_cond,
-    "less": less_cond
-}
-
-variables = {}
-code = ["sequence", 
-        ["set", "i", 500000],
-        ["set", "fizz", 100000],
-        ["if", ["less", ["div", "i", 1000], "fizz"], ["sequence", ["output", "i"]]]
-    ]
-
-exec_sequence(code)
