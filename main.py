b7081b57e3e3fd1682b66e672605856d9a2367b2
Slaytt <117762113+Slaytt@users.noreply.github.com>
2023-11-16 11:12:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from interprete import *
+from lexique import *
 def main():
-    code =  ["sequence", 
-            ["set", "i", 500],
-            ["set", "fizz", 100],
-            ["if", ["less", ["div", "i", 1000], "fizz"], 
-                ["sequence", ["output", "i"]]]
-            ]
+    with open("shell.txt",encoding = 'utf-8') as file: 
+        code = analyse(file.readlines())
+    print(code)
     test = interprete()
-    #with open("shell",encoding = 'utf-8') as file: 
     test.exec_sequence(code)
-
+    
+   
+ 
 if __name__== '__main__' :
     main()
 
